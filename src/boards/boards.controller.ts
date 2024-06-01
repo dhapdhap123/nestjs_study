abb90edad1cac4286b637c5bb353c8f6c389716a
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2024-06-01 18:44:00 +0900

    주석 제거 및 parseIntPipe 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Put,
@@ -15,17 +16,13 @@ import { BoardsService } from './boards.service';
 import { CreateBoardDTO } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 import { BoardStatus } from './board-status.type';
-import { BoardRepository } from './boards.repository';
 import { Board } from './board.entity';
 import { UpdateBoardDTO } from './dto/update-board.dto';
 import { DeleteBoardResponse } from './dto/response/delete-board.response';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(
-    private boardRepository: BoardRepository,
-    private boardsService: BoardsService,
-  ) {}
+  constructor(private boardsService: BoardsService) {}
 
   @Post()
   @UsePipes(ValidationPipe)
@@ -39,14 +36,14 @@ export class BoardsController {
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: number): Promise<Board> {
+  getBoardById(@Param('id', ParseIntPipe) id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
 
   @Patch('/:id/status')
   @UsePipes(ValidationPipe)
   updateBoardStatus(
-    @Param('id') id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Promise<Board> {
     const updatedBoard = this.boardsService.updateBoardStatus(id, status);
@@ -57,7 +54,7 @@ export class BoardsController {
   @Patch('/:id/update')
   @UsePipes(ValidationPipe)
   updateBoard(
-    @Param('id') id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body() updateBoardDTO: UpdateBoardDTO,
   ): Promise<Board> {
     const updatedBoard = this.boardsService.updateBoard(id, updateBoardDTO);
@@ -66,31 +63,10 @@ export class BoardsController {
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id') id: number): Promise<DeleteBoardResponse> {
+  deleteBoard(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<DeleteBoardResponse> {
     const result = this.boardsService.deleteBoard(id);
     return result;
   }
-  // @Get()
-  // getAllBoard(): Board[] {
-  //   return this.boardsService.getAllBoards();
-  // }
-
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDTO: CreateBoardDTO): Board {
-  //   return this.boardsService.createBoard(createBoardDTO);
-  // }
-
-  // @Delete('/:id')
-  // deleteBoard(@Param('id') id: string): void {
-  //   this.boardsService.deleteBoard(id);
-  // }
-
-  // @Patch('/:id/status')
-  // updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  // ): Board {
-  //   return this.boardsService.updateBoardStatus(id, status);
-  // }
 }
